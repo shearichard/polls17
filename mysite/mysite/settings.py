69e32b7ce0b3154ac9dea10ce915884985def404
Richard Shea <rshea@thecubagroup.com>
2015-02-12 17:04:58 +1300

    The Whitenoise utility requires both STATIC_ROOT and STATIC_URL, this change provides that

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -87,4 +87,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
