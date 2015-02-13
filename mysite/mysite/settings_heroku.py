87917211df1fe24b71eef515706dedd4f51d49fe
Richard Shea <rshea@thecubagroup.com>
2015-02-13 23:30:00 +1300

    Tidy up settings_heroku.py to make it a little more readable

--- mysite/mysite/settings_heroku.py
+++ mysite/mysite/settings_heroku.py
@@ -3,13 +3,6 @@ from .settings import *
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
-# Enable Connection Pooling
-#DATABASES['default']['ENGINE'] = 'django_postgrespool'
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
@@ -17,9 +10,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
-
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
